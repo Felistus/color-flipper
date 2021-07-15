2dac8eb8ac55ec3f15d0fab6889668cd715091b9
unknown <felistusobieze@gmail.com>
2021-07-15 11:24:16 +0100

    modified the app.js file for the simple background color flip

--- js/app.js
+++ js/app.js
@@ -1,1 +1,14 @@
-const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
+const colors = ["green", "hsl(30, 100%, 50%)", "red", "rgba(133,122,200)", "#f15025", "hsl(225, 100%, 50%)"];
+const clickMeBtn = document.querySelector(".btn");
+
+clickMeBtn.addEventListener("click", () => {
+    let randomIndex = getColorIndex();
+
+    document.querySelector(".color").innerHTML = colors[randomIndex];
+    document.body.style.backgroundColor = colors[randomIndex];
+    document.querySelector(".simple").style.color = colors[randomIndex];
+});
+
+let getColorIndex = () => {
+    return Math.floor(Math.random() * colors.length)
+}
